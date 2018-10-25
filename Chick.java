b801a8da6b5d24f5e49e828b235a1bc57629df14
Alexandria Yip <AlYip2@sfusd.edu>
2018-10-25 09:49:42 -0700

    final

--- Chick.java
+++ Chick.java
@@ -1,4 +1,29 @@
-class Chick //implements Animal 
+class Chick implements Animal
 {     
-     //your code here
+  private String myType;
+  private String mySound;      
+  private String mySoundSecond;
+  public Chick (String type, String sound, String soundSecond) {         
+    myType = type;         
+    mySound = sound;
+    mySoundSecond = soundSecond;
+  }
+  
+  public Chick() {         
+    myType = "unknown";         
+    mySound = "unknown";
+  }      
+  
+public String getSound() {
+  double a = Math.random();
+  if (a < 0.5) {
+    return mySound;
+  } else {
+    return mySoundSecond;
+  }
+}     
+
+  public String getType() {
+    return myType;
+  }
 }
